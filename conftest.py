2efbcfe466c1246f2d9cfc79959a155542ec6ec7
jaafertamimmi-design <jaafertamimmi@gmail.com>
2026-09-14 10:19:45 +0300

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
-import _sqlalchemy_stub
-_sqlalchemy_stub.install()
